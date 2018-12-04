d2bb76c1217043b43750f6dd71b92f62c7e89eed
Ernesto Martinez-Baez <ebaezcheco@gmail.com>
2018-12-04 12:02:55 -0800

    added extra atom printing in xyz

--- carve_cluster.py
+++ carve_cluster.py
@@ -30,7 +30,10 @@ print('In this code you need to INPUT: ' \
 infile = open(sys.argv[1], 'r')
 #print('How many molecules you want the cluster to be formed of \n')
 mol_number = sys.argv[2]
-l_atom = sys.argv[3]
+l_atom = sys.argv[3]        #Label of the central atom
+extra_atom = sys.argv[4]    #Add an extra atom label to the xyz / yes or no
+e_atom = sys.argv[5]        #Label of the extra atom to add in each cluster or type no
+
 print(f'Working on file {infile.name}')
 
 #-----------------------------------------------------------------------------#
@@ -51,14 +54,16 @@ try:
     os.remove(outf_name)
     print(f'Previous file {outf_name} was removed')
 except OSError:
-    print(f'Output file didn"t exist before')
+    print(f"Output file didn't exist before")
     pass
 os.system(f'mkdir cluster_N_{mol_number}')
     
 outfile_b = open(outf_name, 'ab')
+
 #-----------------------------------------------------------------------------#
-#Loop over snapshots, over element types to form molecules based on number of
-#molecules requested. 
+# Loop over snapshots, over element types to form molecules based on number of
+# molecules requested.
+
 for i in range(snaps):
     snap_xyz = []
     d = []
@@ -67,14 +72,15 @@ for i in range(snaps):
             continue
         snap_xyz.append(infile_lines[i*(N+2)+j].split())                        
     dataf_xyz = pd.DataFrame(data = snap_xyz, columns = ['Label', 'X', 'Y', 'Z'], dtype = float)
+    print(dataf_xyz[dataf_xyz['Label'] == 'Li'].values)
     l = np.zeros((len(dataf_xyz[dataf_xyz['Label']=='O']), 2), dtype=float)
     for j in range(len(dataf_xyz[dataf_xyz['Label']=='O'])):
         distance = math.sqrt(math.pow(dataf_xyz[dataf_xyz['Label'] == 'O'].iloc[j]['X']-dataf_xyz[dataf_xyz['Label'] == l_atom]['X'], 2) + \
                    math.pow(dataf_xyz[dataf_xyz['Label'] == 'O'].iloc[j]['Y']-dataf_xyz[dataf_xyz['Label'] == l_atom]['Y'], 2) + \
                    math.pow(dataf_xyz[dataf_xyz['Label'] == 'O'].iloc[j]['Z']-dataf_xyz[dataf_xyz['Label'] == l_atom]['Z'], 2)  )
-        l[j][0] = dataf_xyz[dataf_xyz['Label']=='O'].index[j] 
+        l[j][0] = dataf_xyz[dataf_xyz['Label']=='O'].index[j]
         l[j][1] = distance
-#    print(np.shape(l))
+#   print(np.shape(l))
     d_array = l[l[:,1].argsort()]
     p = np.zeros((len(dataf_xyz[dataf_xyz['Label']=='H']), 2), dtype=float)
     cluster = pd.DataFrame(columns = ['Label','X', 'Y', 'Z'])
@@ -84,26 +90,35 @@ for i in range(snaps):
             o_h_distance = math.sqrt(math.pow(dataf_xyz[dataf_xyz['Label'] == 'H'].iloc[m]['X']-dataf_xyz.iloc[int(d_array[:][k][0])]['X'], 2) + \
                            math.pow(dataf_xyz[dataf_xyz['Label'] == 'H'].iloc[m]['Y']-dataf_xyz.iloc[int(d_array[:][k][0])]['Y'], 2) + \
                            math.pow(dataf_xyz[dataf_xyz['Label'] == 'H'].iloc[m]['Z']-dataf_xyz.iloc[int(d_array[:][k][0])]['Z'], 2)  )
-            p[m][0] = dataf_xyz[dataf_xyz['Label']=='H'].index[m] 
+            p[m][0] = dataf_xyz[dataf_xyz['Label']=='H'].index[m]
             p[m][1] = o_h_distance
 #        print(np.shape(p))
         d_array_H = p[p[:,1].argsort()]
         s = []
         for o in range(len(d_array_H[d_array_H[:,1] < 1.2][:,0])):
             s.append(int(d_array_H[d_array_H[:,1] < 1.2][:,0][o]))
-        b = int(d_array[:][k][0])    
+        b = int(d_array[:][k][0])
 #        print(type(dataf_xyz.iloc[[b]]), '\n', type(dataf_xyz.iloc[s]))
 #        print(dataf_xyz.iloc[[b]], '\n', dataf_xyz.iloc[s])
         cluster = cluster.append(dataf_xyz.iloc[[b]], ignore_index = True)
         cluster = cluster.append(dataf_xyz.iloc[s], ignore_index = True)
-#-----------------------------------------------------------------------------#
-#Imprime data the l_atom, O, and H to form requested number of molecules cluster
+# -----------------------------------------------------------------------------#
+# Imprime data the l_atom, O, and H to form requested number of molecules in the cluster
 #    print(type(cluster))
-    with open(outf_name, 'a+') as outfile_s:
-        outfile_s.write(f'{len(cluster)} \n\n')
-    with open(outf_name, 'ab') as outfile_b:
-        np.savetxt(outfile_b, cluster.values, fmt='%s')
-    
+# Adding an extra atom label with its coordinates
+    if extra_atom == 'yes':
+        N_atom = len(cluster) + 1
+        with open(outf_name, 'a+') as outfile_s:
+            outfile_s.write(f'{N_atom} \n\n')
+        with open(outf_name, 'ab') as outfile_b:
+            np.savetxt(outfile_b, cluster.values, fmt='%s')
+            np.savetxt(outfile_b, dataf_xyz[dataf_xyz['Label'] == e_atom].values, fmt='%s')
+    else:
+        N_atom = len(cluster)
+        with open(outf_name, 'a+') as outfile_s:
+            outfile_s.write(f'{N_atom} \n\n')
+        with open(outf_name, 'ab') as outfile_b:
+            np.savetxt(outfile_b, cluster.values, fmt='%s')
 
 infile.close()
 os.system(f'mv {outf_name} cluster_N_{mol_number}/.')
