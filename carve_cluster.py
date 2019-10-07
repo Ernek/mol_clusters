2dc6020e68d01a28bf68f977fac0c6444af82ace
Ernesto Martinez-Baez <ebaezcheco@gmail.com>
2019-10-07 09:57:18 -0700

    fix quotations

--- carve_cluster.py
+++ carve_cluster.py
@@ -53,7 +53,7 @@ else:
 snaps = int(len(infile_lines)/(N+2))
 print(f'There are {snaps} snapshots in the {infile.name} file')
 
-outf_name = f"cluster_N_{mol_number}.xyz""
+outf_name = f"cluster_N_{mol_number}.xyz"
 try:
     os.remove(outf_name)
     print(f'Previous file {outf_name} was removed')
